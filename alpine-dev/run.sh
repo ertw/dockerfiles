d09ad83a3f9ae18ef7bfdc1e296e0bf63fea7ac7
Erik Williamson <me@erik.tw>
2017-10-14 04:12:06 +0000

    add names to run, fix git pager

--- alpine-dev/run.sh
+++ alpine-dev/run.sh
@@ -1,1 +1,1 @@
-docker run -it --rm --env-file ./.env --mount type=bind,source=/home/core/passenger_roster,target=/root/dev ertw/alpine-dev /bin/zsh
+docker run -it --rm --env-file ./.env --mount type=bind,source=/home/core/passenger_roster,target=/root/dev ertw/alpine-dev --name alpine-dev /bin/zsh
--- dotnet-core-dev/run.sh
+++ dotnet-core-dev/run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -it --rm -p 5000:5000 --mount type=bind,source=/home/core/passenger_roster,target=/app ertw/dotnet-core-dev
+docker run -it --rm -p 5000:5000 --env-file ./.env --mount type=bind,source=/home/core/passenger_roster,target=/app --name dotnet-dev ertw/dotnet-core-dev
